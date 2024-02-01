674f9e3714fb69cd5c48e31fda0fad508bdec46b
anupam-settyl <anupam.s@settyl.com>
2024-02-02 02:07:18 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+/** @type {import('next').NextConfig} */
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+const NextConfig = {
+  ...withNextra(), 
+  output: "standalone",
+}
+
+module.exports = NextConfig
