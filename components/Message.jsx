ab479ae249980efd85198258a7dc2c5a4b469c6a
ROHIT RANJAN SINGH <rohitranjan2702@gmail.com>
2024-02-02 14:19:57 +0530

    Update package name and fix formatting in Chat component

---
+++ components/Message.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+const Message = ({ username, text, time }) => {
+  return (
+    <>
+      <div class="flex items-start gap-2.5">
+        <img
+          class="w-8 h-8 rounded-full"
+          src={`https://ui-avatars.com/api/?name=${username}`}
+          alt="Jese image"
+        />
+        <div class="flex flex-col gap-1 w-full max-w-[320px]">
+          <div class="flex items-center space-x-2 rtl:space-x-reverse">
+            <span class="text-sm font-semibold text-gray-900 dark:text-gary-200">
+              {username}
+            </span>
+            <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+              {time}
+            </span>
+          </div>
+          <div class="flex flex-col leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+            <p class="text-sm font-normal text-gray-900 dark:text-white">
+              {text}
+            </p>
+          </div>
+          <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+            Delivered
+          </span>
+        </div>
+        {/* <button
+          id="dropdownMenuIconButton"
+          data-dropdown-toggle="dropdownDots"
+          data-dropdown-placement="bottom-start"
+          class="inline-flex self-center items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-600"
+          type="button"
+        >
+          <svg
+            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 4 15"
+          >
+            <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+          </svg>
+        </button>
+        <div
+          id="dropdownDots"
+          class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 dark:divide-gray-600"
+        >
+          <ul
+            class="py-2 text-sm text-gray-700 dark:text-gray-200"
+            aria-labelledby="dropdownMenuIconButton"
+          >
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Reply
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Forward
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Copy
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Report
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Delete
+              </a>
+            </li>
+          </ul>
+        </div> */}
+      </div>
+    </>
+  );
+};
+
+export default Message;
--- pages/chat/index.jsx
+++ pages/chat/index.jsx
@@ -1,15 +1,17 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import supabase from "../../utils/client"
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc'; // import utc plugin
-import relativeTime from 'dayjs/plugin/relativeTime';
-
+"use client";
+import React, { useEffect, useState } from "react";
+import supabase from "../../utils/client";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc"; // import utc plugin
+import relativeTime from "dayjs/plugin/relativeTime";
+import Message from "../../components/Message";
 
 function Chat() {
-  const [messages, setMessages] = useState([{ "id": 1, "text": "Welcome Here!" }]);
-  const [text, setText] = useState("")
-  const [userName, setUserName] = useState(`user${Math.floor(Math.random() * 100000)}`)
+  const [messages, setMessages] = useState([{ id: 1, text: "Welcome Here!" }]);
+  const [text, setText] = useState("");
+  const [userName, setUserName] = useState(
+    `user${Math.floor(Math.random() * 100000)}`
+  );
   // extend dayjs with the plugins
   dayjs.extend(utc);
   dayjs.extend(relativeTime);
@@ -17,25 +19,26 @@ function Chat() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const dataMsg = text;
-      const { error } = await supabase.from("messages").insert([
-        {
-          text: dataMsg,
-          username: userName,
-        },
-      ]);
-      if (error) {
-        console.error(error.message);
-        return;
+    if (e.key === "Enter") {
+      try {
+        const dataMsg = text;
+        const { error } = await supabase.from("messages").insert([
+          {
+            text: dataMsg,
+            username: userName,
+          },
+        ]);
+        if (error) {
+          console.error(error.message);
+          return;
+        }
+        console.log("Successfully sent!");
+      } catch (error) {
+        console.log("error sending message:", error);
+      } finally {
+        setText("");
       }
-      console.log("Successfully sent!");
-    } catch (error) {
-      console.log("error sending message:", error);
-    } finally {
-      setText('')
     }
-
   };
 
   const fetchMsg = async () => {
@@ -45,70 +48,110 @@ function Chat() {
       .range(0, 49)
       .order("id", { ascending: true });
 
+    setMessages([...data]);
 
-    setMessages([...data])
+    console.log(data, error);
 
-    console.log(data, error)
-
-    supabase.channel('schema-db-changes').on(
-      'postgres_changes',
-      {
-        event: 'INSERT', // Listen only to INSERTs
-        schema: 'public',
-      },
-      (payload) => {
-        setMessages((prev) => [...prev, payload.new])
-        console.log("data", payload)
-      }
-    ).subscribe()
-  }
-
-
+    supabase
+      .channel("schema-db-changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT", // Listen only to INSERTs
+          schema: "public",
+        },
+        (payload) => {
+          setMessages((prev) => [...prev, payload.new]);
+          console.log("data", payload);
+        }
+      )
+      .subscribe();
+  };
 
   useEffect(() => {
-    if (localStorage.getItem('userName'))
-      setUserName(localStorage.getItem('userName'));
-    fetchMsg()
-    console.log(messages)
-  }, [])
+    if (localStorage.getItem("userName"))
+      setUserName(localStorage.getItem("userName"));
+    fetchMsg();
+    console.log(messages);
+  }, []);
 
   const handleUserNameChange = async (e) => {
     setUserName(e.target.value);
-    localStorage.setItem('userName', e.target.value);
-  }
+    localStorage.setItem("userName", e.target.value);
+  };
 
   return (
-    <div className='p-2 w-screen flex justify-start items-center h-screen flex-col space-y-4'>
-      <div className='text-4xl font-bold mt-12'>CHAT APP</div>
-      <div className='w-full'>
-        <label htmlFor="username">Username</label>
-        <input type='text' id='username' name='userName' value={userName} className='ring-1' onChange={handleUserNameChange} />
+    <div className="p-2 w-screen flex justify-start items-center h-screen flex-col space-y-4">
+      <div className="text-4xl font-bold mt-8">CHAT APP</div>
+      <div className="w-full text-center space-x-2">
+        <label htmlFor="username" className="font-semibold">
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="userName"
+          value={userName}
+          className="ring-1"
+          onChange={handleUserNameChange}
+        />
       </div>
-      <div className='text-2xl font-semibold mt-12'>All Messages</div>
-      <div className='w-full h-96 p-2 rounded border-2 border-slate-900 overflow-y-scroll'>
-        {messages.length != 0 ? messages.map((message, index) => (<>
-          <ul key={index} className='w-60 m-2 p-1 flex bg-green-600 rounded border-2 border-slate-900 flex-col'>
-            <li>User:{message.username || "anonymous"}</li>
-            <li>{message.text}</li>
-            <li>{dayjs.utc(message.timestamp).fromNow()}</li>
-          </ul>
-        </>
-        )) : (
-          <>
-            <div className='w-full h-full flex justify-center items-center'>
-              <h1>Welcome To Chat App!</h1>
-            </div>
-          </>
-        )}
-      </div>
-      <input type='text' className='ring-1' value={text} onChange={(e) => setText(e.target.value)} />
-      <div className='flex space-x-3'>
-        {/* <button type='button' className='bg-red-600 px-2 py-1 rounded-md text-white' onClick={Subscribe}>Subscribe</button> */}
-        <button type='button' className='bg-green-600 px-2 py-1 rounded-md text-white' onClick={handleSubmit}>Send</button>
-      </div>
+      <div className="text-2xl font-semibold mt-12">All Messages</div>
+      <div className="px-24 w-full space-y-4">
+        <div className="w-full h-96 p-2 pt-4 rounded-xl ring-1 overflow-y-scroll flex flex-col justify-center items-start">
+          {messages.length != 0 ? (
+            messages.map((message, index) => (
+              <>
+                <div className="my-2" key={index}>
+                  <Message
+                    text={message.text}
+                    time={dayjs.utc(message.timestamp).fromNow()}
+                    username={message.username}
+                  />
+                </div>
+              </>
+            ))
+          ) : (
+            <>
+              <div className="w-full h-full flex justify-center items-center">
+                <h1>Welcome To Chat App!</h1>
+              </div>
+            </>
+          )}
+        </div>
+        <div className="flex space-x-3 w-full px-32">
+          <input
+            type="text"
+            className="ring-1 rounded-2xl w-full px-3"
+            placeholder="Type your message here..."
+            value={text}
+            onKeyDown={handleSubmit}
+            onChange={(e) => setText(e.target.value)}
+          />
 
+          <button type="submit" onClick={handleSubmit}>
+            <div class="w-fit rounded-full bg-green-600 p-3 shadow-2xl drop-shadow-xl backdrop-blur-lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-send text-white"
+              >
+                <path d="m22 2-7 20-4-9-9-4Z" />
+                <path d="M22 2 11 13" />
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
